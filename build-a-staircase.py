61c3196010e52173638dd1efee15d833644e0c83
komalkamal_Er <komalkamal56478@gmail.com>
2025-11-19 01:12:37 +0530

    Add files via upload

---
+++ build-a-staircase.py
@@ -0,0 +1,21 @@
+# n = 5
+# for i in range(n):
+ 
+#     for k in range(i):
+#         print("*", end="")
+ #   print() 
+    
+    
+# n = 5
+# for i in range(n):
+#     for k in range(i):
+#         print("*", end="")
+#     print()
+
+
+
+n = 5 # int(input(" Enter a number : "))
+for i in range(n):
+    for k  in range(i):
+        print("*" , end="")
+    print()
---
+++ measurement convertion.py
@@ -0,0 +1,18 @@
+n = float(input( "Enter the value in inches : "))
+
+footValue = 12 * n
+print("Value in foot is -> ",footValue)
+
+yardValue = 36 * n
+print("Value in yard is -> ",yardValue)
+
+cmValue = 2.54 * n
+print("Value in cm is -> ",cmValue)
+
+meterValue = 39.37 * n
+print("Value in meter is -> ",meterValue)
+
+
+
+
+
